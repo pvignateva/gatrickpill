0bee44ea8f9b894e0f33a86d8ef10c29008ca15c
pvignateva <33483420+pvignateva@users.noreply.github.com>
2019-06-13 08:45:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const munch = ["i wanna m", "I WANNA M", "i want to m", "I WANT TO M", "i waant 
 const uwu = ["uwu", "wuv", "patbot pwease", "patbot ily", "ily patbot"];
 const games = ["sekiro", "SEKIRO", "bloodborne", "blood borne", "BLOODBORNE"];
 const doom = ["DOOM", "doom"];
-const kink = ["kink", "fetish"];
+
 //console reply
 client.on("ready", () => {
   console.log("sir, yes sir!!");
@@ -15,6 +15,7 @@ client.on("ready", () => {
 
 //main
 client.on("message", (message) => {
+	//turn off replying to bots
 	if (message.author.bot) return;
 	//reply to trigger text with text
 	//hewwo - hewwo? hewwoo-o
@@ -24,6 +25,7 @@ client.on("message", (message) => {
 	  if(message.content.startsWith("HEWWO")) {
     message.channel.send( "Hewwo…… H-hewwo? (;•́︿•̀ ;)…………… Hewwo ( ͡° ͜ʖ ͡°)" );
   }else
+	  //stummy
 	  if(message.content.includes("stummy huwt")) {
     message.channel.send( " stummy huwt! stummy huwt! i got a bad tummy mistew cybewpunk! i got go sweepy now!" );
   }else
@@ -31,30 +33,33 @@ client.on("message", (message) => {
 	if(message.content.includes("69")) {
 	message.channel.send("nice");
   }else
+	  //420
 	  if(message.content.includes("420")) {
 	message.channel.send("there s no weed in soviet union...");
   }else
+	  //terf
 	  if(message.content.includes("terf")) {
 	message.channel.send("fuck terfs");
   }else
+	  if(message.content.includes ("andy")) {
+		if(message.content.includes ("teeth")) {
+		message.channel.send("andy did nothing wrong ever");
+	}}else 
+	  //piss
 	  if(message.content.includes("piss")) {
 	message.channel.send("...piss? no, knock it off, kids. i have an allergy");
   }else
+	  //its soup
 	 if(message.content.startsWith("its,")) {
     message.channel.send("its, soup");
   }else
-	 if(message.content.includes(",,,,,")) {
-	message.channel.send("its, soup");
-  }else
-	
+	 
 	 //2 conditions
 	if(message.content.includes ("is")) {
 		if(message.content.includes ("good?")) {
 		message.channel.send("boyp howduy");
 	}}else  
-		if( kink.some(word => message.content.includes(word)) ) {
-	message.channel.send("nasty");
-}else
+		
 	//reply with emoji
 
 	//validation
@@ -62,10 +67,7 @@ client.on("message", (message) => {
 		if(message.content.includes ("please")) {
 		message.react("586593863200931890");
 	}}else 	
-		if(message.content.includes ("andy")) {
-		if(message.content.includes ("teeth")) {
-		message.channel.send("andy did nothing wrong ever");
-	}}else 
+		
 	//opression
 	if(message.content.includes ("oppress andy")) {
 	message.react("586577717990916097");
