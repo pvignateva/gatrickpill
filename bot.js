d404635bc075859119b73ea593ef53d95dfb68db
pvignateva <33483420+pvignateva@users.noreply.github.com>
2019-06-13 06:57:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,35 @@
+//important header
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+//console reply
+client.on("ready", () => {
+  console.log("sir, yes sir!!");
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+//main
+client.on("message", (message) => {
+	//reply to trigger text with text
+	//hewwo - hewwo? hewwoo-o
+	if(message.content.startsWith("hewwo")) {
+    message.channel.send("H…hewwo? Is anybody out here? Hewwo??? Hewwo?!?!?");
+  }else
+	// 69 - nice
+	if(message.content.includes("69")) {
+	message.channel.send("nice");
+  }else
+	//reply with emoji
+	//validation
+	if(message.content.includes ("validation")) {
+		if(message.content.includes ("please")) {
+		message.react("586593863200931890");
+	}}else 	
+	//opression
+	if(message.content.includes ("oppress andy")) {
+	message.react("586577717990916097");
+	}
+		
 });
-
- 
-
-// THIS  MUST  BE  THIS  WAY
+//important ending
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
