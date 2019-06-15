1898890ff43ffe8b733e9f5d04ee11bbb32c51ad
pvignateva <33483420+pvignateva@users.noreply.github.com>
2019-06-15 07:26:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const acab = ["cop", "police"];
 const hewwo = ["Hewwo", "hewwo", "HEWWO"];
 const doyouyearn = ["you ever yearn?", "u ever yearn", "Do you ever yearn?", "do you yearn", "do u yearn"];
 const haveyouyearned = ["Have you yearned?", "u yearned", "you yearned?"];
-const piss = ["piss"];
+const pee = ["piss", "pee"];
 
 //randomizer for hewwo
 function randomhewwo(){
@@ -161,7 +161,7 @@ client.on("message", (message) => {
 	}else
 	
 	//pee
-	if(piss.some(word => message.content.includes(word))){
+	if(pee.some(word => message.content.includes(word))){
 		message.channel.send(randompiss());
 	}
 	
