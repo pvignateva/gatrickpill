b31205db08df09395e55a284b97f5fd43d083761
pvignateva <33483420+pvignateva@users.noreply.github.com>
2019-06-15 04:58:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,20 @@ const client = new Discord.Client();
 
 //randomizer for ping
 function randomswear(){
-	var randomNumber = Math.round(Math.random()*3); // 0, 1 or 2
+	var randomNumber = Math.round(Math.random()*11); 
 	switch(randomNumber){
 		case 0: return 'fuck';
 		case 1: return 'shit';
-		case 2: return 'asshole';
-		case 3: return 'bastard';
+		case 2: return 'moistey bastard';
+		case 3: return 'oh fuck off';
+		case 4: return 'dickhead';
+		case 5: return 'piss off';
+		case 6: return 'you nasty asshole';
+		case 7: return 'dumbass bastard';
+		case 8: return 'bloody hell';
+		case 9: return 'wanker';
+		case 10: return 'pissless bastard boy';
+		case 11: return 'get fucked';
 	}
 }
 
